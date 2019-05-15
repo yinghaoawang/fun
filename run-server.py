2e2c83ee15d6e850b5a13289dfc2c45f77b1f977
yinghaoawang <yinghao.a.wang@gmail.com>
2019-05-15 06:44:49 -0700

    changed os env str to int for heroku

--- run-server.py
+++ run-server.py
@@ -2,7 +2,8 @@ import os
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 
 os.chdir('./public')
-PORT = os.getenv('PORT') or 9000;
+PORT = os.getenv('PORT') or '9000'
+PORT = int(PORT)
 server_address = ('', PORT);
 httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
 print('Listening on port {}'.format(PORT))
